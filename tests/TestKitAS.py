8d1e9d70558f9190402caabba49a272472991c52
ahhon <polygon2000x@gmail.com>
2021-09-26 18:57:47 +0700

    threw away an unnecessary class in t_connection

--- tests/TestKitAS.py
+++ tests/TestKitAS.py
@@ -2,19 +2,26 @@ from handyPyUtil.tests import TestKit
 from server import Server
 
 DFLT_TEST_PORT = 5492
+DFLT_TEST_ADDR = f"127.0.0.1"
 
 class TestKitAS(TestKit):
-    def __init__(self, *arg, **kwarg):
+    def __init__(self, addr=DFLT_TEST_ADDR, port=DFLT_TEST_PORT, *arg, **kwarg):
+        self.addr = addr
+        self.port = port
         super().__init__(*arg, **kwarg)
 
-    def startServer(self, port=DFLT_TEST_PORT, **pkwarg):
+    def startServer(self, **pkwarg):
         def srvTarget(**kwargs):
             srv = Server()
-            srv.run(addr="127.0.0.1", port=kwargs['port'])
+            srv.run(addr=kwargs['addr'], port=kwargs['port'])
 
         srvKwarg = {
-            'port': port,
+            'addr': self.addr,
+            'port': self.port,
         }
 
         p = self.forkProcess(srvTarget, args=(), kwargs=srvKwarg, **pkwarg)
         return p
+
+    def getUri(self):
+        return f"http://{self.addr}:{self.port}"
